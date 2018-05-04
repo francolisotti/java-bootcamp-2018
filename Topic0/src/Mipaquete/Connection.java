bf70d9ec4b48935e9f1e379b98669ffd47758619
Franco Lisotti <francolisotti96@gmail.com>
2018-05-04 06:42:18 -0300

    topic0
    topic0

---
+++ Topic0/src/Mipaquete/Connection.java
@@ -0,0 +1,12 @@
+package Mipaquete;
+
+public class Connection {
+	public String getConnection()
+	{
+		return "Conexion exitosa";
+	}
+	
+	public boolean isClosed(){
+		return true;
+	}
+}
---
+++ Topic0/src/Mipaquete/DAOExample.java
@@ -0,0 +1,34 @@
+package Mipaquete;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DAOExample {
+	private static DAOExample instance;
+    private Connection connection;
+    private String url = "jdbc:postgresql://localhost:5432/jdbc";
+    private String username = "root";
+    private String password = "localhost";
+
+    private DAOExample() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static DAOExample getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DAOExample();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new DAOExample();
+        }
+
+        return instance;
+}
