ee1c3ac3aa2b4a2bb1324db4cd37ff7d56e78c2d
Franco Lisotti <francolisotti96@gmail.com>
2018-05-10 19:38:31 -0300

    topic2
    first part finished

--- FrancoLisotti/src/main/java/Topic2/App.java
+++ FrancoLisotti/src/main/java/Topic2/App.java
@@ -1,5 +1,7 @@
 package Topic2;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,31 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        RecentFileList milista= new RecentFileList();
+        int rta=-1;
+
+        while (rta!=0){
+
+            System.out.println("\nIngrese numero");
+            System.out.println("1 - Agregar un archivo");
+            System.out.println("0 - Salir");
+            Scanner sc = new Scanner(System.in);
+            rta = sc.nextInt ();
+            String archivo;
+
+            switch (rta) {
+                case 1:
+                    System.out.println("Ingrese nombre del archivo y extension a agregar");
+                    sc = new Scanner(System.in);
+                    archivo = sc.nextLine();
+                    milista.addFile(archivo);
+                    break;
+                case 0:
+                    break;
+            }
+
+        }
+
+
     }
 }
