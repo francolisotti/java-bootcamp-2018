b19f76d2fe9189a29545448529626b0392f10d55
Franco Lisotti <francolisotti96@gmail.com>
2018-05-12 11:09:07 -0300

    topic2 finished

--- Topic2.2/src/main/java/secondPart/Post.java
+++ Topic2.2/src/main/java/secondPart/Post.java
@@ -15,16 +15,4 @@ public class Post {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
 }
